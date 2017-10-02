f419a1bcdb9b6ad0b1a643b9ecd329adad31551a
NekoTale <drumchik2009@yandex.ru>
2017-10-02 15:05:49 +0300

    alala

---
+++ src/main/java/com/dimon/bashorg/core/LinkedList.java
@@ -0,0 +1,24 @@
+package Core;
+
+/**
+ * Created by mds on 02.10.17.
+ */
+public class MyLinkedList<T> (T newObj){
+        T ObjProc = newObj;
+        T ObjPrev = null;
+        T ObjNext = null;
+
+
+
+
+    void add (final T ObjProc){
+        this.Obj
+
+        }
+
+    LinkedList my = new LinkedList;
+
+
+
+
+}
